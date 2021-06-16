7355e1c7df663ed1e4474a40f3f0e9d78d02df8f
MostafaFarag14 <mostafafarag140@gmail.com>
2021-06-16 20:07:27 +0200

    playing with grids

--- layout/about.js
+++ layout/about.js
@@ -9,20 +9,20 @@ function About() {
           <div>
             <h3>YOU'RE GOING TO FALL IN LOVE WITH NATURE</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum
-            sapiente aspernatur libero repellat quis consequatur ducimus quam nisi
-            exercitationem omnis earum qui.</p>
+              sapiente aspernatur libero repellat quis consequatur ducimus quam nisi
+              exercitationem omnis earum qui.</p>
           </div>
           <div>
             <h3>LIVE ADVENTURES LIKE YOU NEVER HAVE BEFORE</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla
-            deserunt voluptatum nam.</p>
+              deserunt voluptatum nam.</p>
           </div>
-          <a>Learn more &rarr;</a>
+          <a href='#'>Learn more &rarr;</a>
         </div>
         <div className='gallery'>
-          <img src='/img/nat-1.jpg' />
-          <img src='/img/nat-2.jpg' />
-          <img src='/img/nat-3.jpg' />
+          <img className='nat1' src='/img/nat-1.jpg' />
+          <img className='nat2' src='/img/nat-2.jpg' />
+          <img className='nat3' src='/img/nat-3.jpg' />
         </div>
       </div>
     </section>
